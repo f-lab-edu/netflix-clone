037b8adbaec7fdcf0440e8a76fe9e51e4f2f749f
Big One <sponjjanc@naver.com>
2023-09-22 20:49:52 +0900

    feat: email-form component

---
+++ src/components/login/email-form.js
@@ -0,0 +1,23 @@
+import { forwardRef } from "react";
+
+export default forwardRef(function EmailForm(props, ref) {
+  return (
+    <form onSubmit={props.onSubmit}>
+      <h3>
+        시청할 준비가 되셨나요? 멤버십을 등록하거나 재시작하려면 이메일 주소를
+        입력하세요.
+      </h3>
+      <div>
+        <input
+          ref={ref}
+          type="text"
+          placeholder="이메일 주소를 입력해주세요."
+          className="input input-bordered inline-block text-black w-full max-w-xs"
+        />
+        <button className="btn inline-block ml-3 bg-red-500 btn-outline">
+          시작하기
+        </button>
+      </div>
+    </form>
+  );
+});
