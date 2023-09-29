fbcd7ab61aa7ef14d4c41cc42cca0676d67ee327
Big One <sponjjanc@naver.com>
2023-09-29 19:08:33 +0900

    feat: customized images default sizes && remotePatterns add request site

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    sizes: "200px",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+      },
+      {
+        protocol: "https",
+        hostname: "occ-0-7129-3996.1.nflxso.net",
+      },
+      {
+        protocol: "https",
+        hostname: "daisyui.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
