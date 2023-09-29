6f06282d3e759b1d0fb46ee44dd0a41e87002cf9
Big One <sponjjanc@naver.com>
2023-09-29 19:04:14 +0900

    feat: card-item 클릭시 상세페이지 이동 //alert로 변경 예정

--- src/components/browse/card-item.js
+++ src/components/browse/card-item.js
@@ -1,14 +1,22 @@
-function CardItem({ id, title, overview, releaseDate, posterPath }) {
-  return (
-    <div className="card w-96 ml-2 bg-white text-black">
-      {/*<h1>{title}</h1>*/}
-      {/*<div>*/}
-      {/*  <p>{releaseDate}</p>*/}
-      {/*  <p>{overview}</p>*/}
-      {/*</div>*/}
-      <img src={`https://image.tmdb.org/t/p/w500/${posterPath}`} alt="Shoes" />
-    </div>
-  );
-}
+import Image from "next/image";
+import { forwardRef } from "react";
+import Link from "next/link";
 
-export default CardItem;
+export default forwardRef(function CardItem({ id, backdropPath }, ref) {
+  return (
+    // div 에 alert 크게 띄워서 alert 에 `/contents/${id}`페이지 출력 되게
+    <Link
+      href={`/contents/${id}`}
+      ref={ref}
+      className="carousel-item ml-2 bg-white text-black cursor-pointer"
+    >
+      <Image
+        id={id}
+        src={`https://image.tmdb.org/t/p/w500${backdropPath}`}
+        alt="poster"
+        width={300}
+        height={150}
+      />
+    </Link>
+  );
+});
