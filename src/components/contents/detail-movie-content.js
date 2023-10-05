4e8e841a437278e28add39e4d61b05ad7eb1805d
Big One <sponjjanc@naver.com>
2023-10-06 01:22:41 +0900

    feat: 티저 영상 요청 및 송출

--- src/components/contents/detail-movie-content.js
+++ src/components/contents/detail-movie-content.js
@@ -1,37 +1,30 @@
-import Image from "next/image";
-import { getDetailContent } from "../../app/api/contents/route";
+import { getDetailContent, getVideos } from "../../app/api/contents/route";
 async function getContent(id) {
   const content = await getDetailContent(id).then((data) => data.json());
-
   return content;
 }
-
+async function getTeaser(id) {
+  const videos = await getVideos(id).then((data) => data.json());
+  return videos;
+}
+// TODO: 추 후 detail-movie-content, detail-TV-content를  공통 파일로 합치고
+// content-info를 movie-content-info, tv-content-info로 나누어야함
 async function DetailMovieContent({ id }) {
-  const content = await getContent(id);
+  const videos = await getTeaser(id);
+  const key = videos[0]?.key ?? "";
+  const videoSrc = `https://www.youtube.com/embed/${key}?autoplay=1&loop=1&playlist=${key}&mute=1&controls=0&si=nh5lkzt8Jqa2v4Z1&amp;`;
+
   return (
     <>
-      <div>
-        Content Detail Page.
-        <h1> 제목: {content.title}</h1>
-        <Image
-          src={`https://image.tmdb.org/t/p/w500${content.backdrop_path}`}
-          alt={"poster"}
-          width={500}
-          height={500}
-        />
-        <div>
-          <div>출시일 : {content.release_date}</div>
-          <div>상영 시간: {content.runtime}</div>
-          <div>
-            지원 언어: [
-            {content.spoken_languages.map((l) => l.english_name + " ")}]
-          </div>
-          <div>
-            평점: {content.vote_average.toFixed(2)}({content.vote_count})
-          </div>
-          <p>장르: {content.genres.map((g) => g.name + " ")}</p>
-          <p>{content.overview}</p>
-        </div>
+      <div className={"w-full h-[40rem] overflow-hidden relative"}>
+        <iframe
+          className={"w-full h-full"}
+          src={videoSrc}
+          title="YouTube video player"
+          frameBorder={0}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
       </div>
     </>
   );
