c6428f0dbbc5f709ed9c908f178e1feb59b1e75e
Big One <sponjjanc@naver.com>
2023-10-06 01:25:17 +0900

    feat: 영화 여부에 따른 요청 url 설정

--- src/components/browse/card-item.js
+++ src/components/browse/card-item.js
@@ -2,11 +2,15 @@ import Image from "next/image";
 import { forwardRef } from "react";
 import Link from "next/link";
 
-export default forwardRef(function CardItem({ id, backdropPath }, ref) {
+export default forwardRef(function CardItem(
+  { id, backdropPath, isMovie },
+  ref,
+) {
+  const href = `${id}?isMovie=${isMovie}`;
   return (
     // div 에 alert 크게 띄워서 alert 에 `/contents/${id}`페이지 출력 되게
     <Link
-      href={`/contents/${id}`}
+      href={`/contents/${href}`}
       ref={ref}
       className="carousel-item ml-2 bg-white text-black cursor-pointer"
     >
