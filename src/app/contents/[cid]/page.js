d6a1dd3f3d5b86733304180445cdb109d615d547
Big One <sponjjanc@naver.com>
2023-10-08 01:34:53 +0900

    feat: 콘텐츠 상세페이지 모두 컴포넌트로 분리

--- src/app/contents/[cid]/page.js
+++ src/app/contents/[cid]/page.js
@@ -1,19 +1,9 @@
-"use client";
-import DetailTvContent from "../../../components/contents/detail-tv-content";
-import { useSearchParams } from "next/navigation";
-import DetailMovieContent from "../../../components/contents/detail-movie-content";
+import DetailContent from "../../../components/contents/detail-content";
 
 function ContentDetailPage({ params }) {
-  const searchParams = useSearchParams();
-  const isMovie = searchParams.get("isMovie");
-
-  if (isMovie === "true") {
-    return <DetailMovieContent id={params.cid} />;
-  }
-
   return (
     <>
-      <DetailTvContent id={params.cid} />
+      <DetailContent id={params.cid} />
     </>
   );
 }
