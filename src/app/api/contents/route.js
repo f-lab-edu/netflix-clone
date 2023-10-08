da48c2c362fea9008e37a27d9e55217661cbe172
Big One <sponjjanc@naver.com>
2023-10-09 01:19:20 +0900

    feat: getSeasons route 추가 && contents 구분을 위한 type 추가

--- src/app/api/contents/route.js
+++ src/app/api/contents/route.js
@@ -8,6 +8,9 @@ export async function getDetailContent(id, contentsType, language = "ko") {
   )
     .then((response) => response.json())
     .catch((err) => console.error(err));
+
+  res.contentsType = contentsType;
+
   return NextResponse.json(res);
 }
 
@@ -20,4 +23,15 @@ export async function getTrailersContent(id, contentType, language = "ko") {
     .catch((err) => console.log(err));
 
   return NextResponse.json(res.results);
+}
+
+export async function getSeasons(seriesId, seasonNumber, language = "ko") {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/tv/${seriesId}/season/${seasonNumber}?language=${language}`,
+    options,
+  )
+    .then((response) => response.json())
+    .catch((err) => console.log(err));
+
+  return NextResponse.json(res);
 }
