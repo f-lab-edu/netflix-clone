6807bbe59591213873a7416f2399061d6a716055
Big One <sponjjanc@naver.com>
2023-10-13 14:46:23 +0900

    feat: 체크모양 , 동그라미안에 체크모양 Icon

---
+++ src/components/icon/index.tsx
@@ -0,0 +1,39 @@
+export function CheckInCircle() {
+  return (
+    <div className={"checkbox mt-[100px] mb-16"}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6 stroke-red-600 stroke-2"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export function CheckedIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="w-6 h-6 stroke-red-600 stroke-2 mr-1"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 12.75l6 6 9-13.5"
+      />
+    </svg>
+  );
+}
