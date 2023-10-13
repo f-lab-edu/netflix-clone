7c60a32bbb13945bd8b47c5b9fb2104303b4d392
Big One <sponjjanc@naver.com>
2023-10-13 14:15:38 +0900

    feat: 특정 콘텐츠의 시리즈 조회 페이지

---
+++ src/app/(main-nav)/series/[id]/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import SeasonContents from "../../../../components/seasons/season-contents";
+import { useSearchParams } from "next/navigation";
+
+function SelectedSeriesPage({ params }) {
+  const searchParams = useSearchParams();
+  const contentsType = searchParams.get("contentsType");
+
+  return (
+    <>
+      <div>
+        <SeasonContents id={params.id} contentsType={contentsType} />
+      </div>
+    </>
+  );
+}
+
+export default SelectedSeriesPage;
