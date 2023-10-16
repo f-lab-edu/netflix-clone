282765b37c40f3358a26820a2ea6fe7104f5c758
Big One <sponjjanc@naver.com>
2023-10-16 14:24:33 +0900

    feat: 스타일 속성 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+const config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      width: {
+        "108": "27rem",
+      },
+      height: {
+        "80vh": "80vh",
+      },
+      gridTemplateColumns: {
+        "preview-detail": "minmax(0,2fr) minmax(0,1fr)",
       },
     },
   },
   plugins: [require("daisyui")],
-}
-export default config
+};
+export default config;
