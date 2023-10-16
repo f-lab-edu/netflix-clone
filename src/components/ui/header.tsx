95a4cb29d6c69b422b5768c220e6ef4f4d9512a9
Big One <sponjjanc@naver.com>
2023-10-16 14:22:23 +0900

    feat: 분기 조건 추가

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,16 +1,23 @@
 "use client";
 import { usePathname } from "next/navigation";
-import InitialNav from "@/components/ui/initial-nav";
-import MainNav from "@/components/ui/main-nav";
+import InitialHeader from "@/components/ui/initial-header";
+import LoggedInHeader from "@/components/ui/logged-in-header";
 
 function Header() {
   const pathname = usePathname();
-  // TODO: ADD 조건 - 세션이 있거나 메인 페이지가 아닐 경우
-  if (pathname === "/") {
-    return <InitialNav />;
-  } else {
-    return <MainNav />;
+
+  // TODO: ADD 조건 - 세션이 있거나 메인 페이지가 아닐 경우 //  수정1 -> 로그인(세션)이 유효한 경우만 LoggedInHeader
+  // 그럼 ... 바꿔야하나 파일이름도 InitialHeader 가 아닌 기본? general? basic   //  LoggedInHeader 는  LoggedIn?
+  if (
+    pathname === "/" ||
+    pathname === "/signup" ||
+    pathname === "/login" ||
+    pathname === "/signup/regform"
+  ) {
+    return <InitialHeader />;
   }
+
+  return <LoggedInHeader />;
 }
 
 export default Header;
