1306f96c4f8177a3ba129304af08a20a63c701a2
Big One <sponjjanc@naver.com>
2023-10-22 23:22:03 +0900

    feat: 시리즈 페이지를 개발했습니다.

--- src/app/(main-nav)/series/page.tsx
+++ src/app/(main-nav)/series/page.tsx
@@ -1,7 +1,29 @@
-function Series() {
+import CardList from "@/components/ui/card-list";
+import { getGenres, getTvContents } from "@/services/contents";
+import OptionList from "@/components/ui/option-list";
+
+async function Series() {
+  const queryParams = {
+    include_adult: true,
+    include_null_first_air_dates: true,
+    language: "ko",
+    sort_by: "popularity.desc",
+    page: 1,
+  };
+  const data = await getTvContents(queryParams).then((res) => res.json());
+  const genres = await getGenres("tv").then((res) => res.json());
+  const selectBoxStyles = "select bg-transparent border-white";
+
   return (
     <>
-      <div>The Series Page</div>
+      <section>
+        <div className={"ml-5 mt-5 mb-10"}>
+          <OptionList options={genres.genres} sStyle={selectBoxStyles} />
+        </div>
+        <div className={"grid gap-4 grid-cols-4 px-5"}>
+          <CardList dataList={data.results} contentsType={data.media_type} />
+        </div>
+      </section>
     </>
   );
 }
