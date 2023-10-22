f8e9eb2d3ed9d35a5567a1f01f6c2984f47b28d7
Big One <sponjjanc@naver.com>
2023-10-23 01:02:55 +0900

    feat: 공통 UI의 타입과 조건을 추가했습니다.

--- src/components/ui/card-item.tsx
+++ src/components/ui/card-item.tsx
@@ -1,16 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function CardItem({ item, contentsType }) {
+export default function CardItem({
+  id,
+  mediaType,
+  posterPath,
+  contentsType,
+}: {
+  id: number;
+  mediaType?: string;
+  posterPath?: string;
+  contentsType?: string;
+}) {
+  const type = mediaType ?? contentsType;
   return (
-    <Link href={`/contents/${item.id}?&contentsType=${contentsType}`}>
+    <Link href={`/contents/${id}?&contentsType=${type}`}>
       <Image
         className={"w-full border rounded cursor-pointer hover:scale-125 "}
-        src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+        src={`https://image.tmdb.org/t/p/w500${posterPath}`}
         alt={"movie-poster"}
         width={500}
         height={500}
-        onMouseOver={null}
         // style={{ width: "100%", height: "auto" }}
       />
     </Link>
