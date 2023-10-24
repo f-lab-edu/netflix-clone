6f85867183231cba4464dee25cd7058a2422081b
Big One <sponjjanc@naver.com>
2023-10-24 21:37:28 +0900

    feat: SEO 를 위해 meta data 의 title 을 지정해주었습니다.

--- src/app/(main-nav)/my-list/page.tsx
+++ src/app/(main-nav)/my-list/page.tsx
@@ -1,5 +1,28 @@
-function MyListPage() {
-  return <div>My List Page</div>;
+import CardList from "@/components/ui/card-list";
+import { getPopularTVProgram } from "@/services/contents";
+import { Metadata } from "next";
+//TODO: data  재정의 Mylist 불러오깁
+export const metadata: Metadata = {
+  title: "내가 찜한 콘텐츠",
+  description: "내가 찜한 콘텐츠",
+};
+async function MyListPage() {
+  const data = await getPopularTVProgram().then((res) => res.json());
+
+  console.log("movies Page: ", data);
+  return (
+    <>
+      <div className={"ml-5 mt-5 mb-10"}>
+        <select className={"select bg-transparent border-white"}>
+          <option>장르</option>
+          <option>ㄴㅁㅇㄴㅁ</option>
+        </select>
+      </div>
+      <div className={"grid gap-4 grid-cols-4 px-5"}>
+        <CardList dataList={data.results} contentsType={data.contentsType} />
+      </div>
+    </>
+  );
 }
 
 export default MyListPage;
