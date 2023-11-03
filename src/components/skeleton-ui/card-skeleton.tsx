bc183e7f26e32b5a2cac700815829a4c1c27e47c
Big One <sponjjanc@naver.com>
2023-11-03 12:23:12 +0900

    fix: 매개변수 타입 지정

--- src/components/skeleton-ui/card-skeleton.tsx
+++ src/components/skeleton-ui/card-skeleton.tsx
@@ -1,4 +1,10 @@
-export default function CardSkeleton({ styles, loading }) {
+export default function CardSkeleton({
+  styles,
+  loading,
+}: {
+  styles: string;
+  loading: string;
+}) {
   return (
     <div
       className={`border rounded flex justify-center items-center ${styles}`}
