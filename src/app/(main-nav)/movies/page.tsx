8cba4d5581ff5bba1bb0eb4e7d6d000fca38c44e
Big One <sponjjanc@naver.com>
2023-11-03 12:02:42 +0900

    refactor: 장르 목록이 먼저 불러와지는 경우 CLS  가 발생하기 때문에 전부 다 불러왔을 시 데이터를 보여주도록 수정

--- src/app/(main-nav)/movies/page.tsx
+++ src/app/(main-nav)/movies/page.tsx
@@ -6,6 +6,7 @@ import { useInfiniteQuery } from "react-query";
 import { useEffect, useRef, useState } from "react";
 import { useObserver } from "@/hooks/use-observer";
 import { Options } from "@/types/ui/types";
+import Loading from "@/app/(main-nav)/movies/loading";
 
 function MoviesPage() {
   const bottom = useRef<HTMLDivElement | null>(null);
@@ -49,23 +50,25 @@ function MoviesPage() {
 
   return (
     <section>
-      <div className={"ml-5 mt-5 mb-10"}>
-        {genres && <OptionList options={genres} sStyle={selectBoxStyles} />}
-      </div>
-      {status === "loading" && <p>불러오는중 </p>}
+      {status === "loading" && <Loading />}
       {status === "error" && (
         <p>에러가 발생했습니다. 잠시후 다시 시도해주세요.</p>
       )}
-      {status === "success" && data && (
-        <div className={"grid gap-4 grid-cols-4 px-5"}>
-          {data?.pages.map((page) => (
-            <CardList
-              key={page.page}
-              dataList={page.results}
-              mediaType={page.media_type}
-            />
-          ))}
-        </div>
+      {status === "success" && data && genres && (
+        <>
+          <div className={"ml-5 mt-5 mb-10"}>
+            <OptionList options={genres} sStyle={selectBoxStyles} />
+          </div>
+          <div className={"grid gap-4 grid-cols-4 px-5"}>
+            {data?.pages.map((page) => (
+              <CardList
+                key={page.page}
+                dataList={page.results}
+                mediaType={page.media_type}
+              />
+            ))}
+          </div>
+        </>
       )}
       <div ref={bottom} className={"flex justify-center my-10"}>
         <span className="loading loading-spinner loading-md" />
