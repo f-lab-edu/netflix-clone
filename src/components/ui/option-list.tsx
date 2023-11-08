7312cb4060f9790620907a636023d96705461539
Big One <sponjjanc@naver.com>
2023-11-08 13:45:12 +0900

    feat: select 변경 감지를 위해 onChange 함수를 추가했습니다.

--- src/components/ui/option-list.tsx
+++ src/components/ui/option-list.tsx
@@ -1,16 +1,19 @@
 import { Options } from "@/types/ui/types";
+import React from "react";
 
 export default function OptionList({
   options,
   sStyle,
   defaultOption,
+  onChange,
 }: {
   options: Options[];
   sStyle: string;
   defaultOption?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }) {
   return (
-    <select className={sStyle}>
+    <select className={sStyle} onChange={onChange}>
       {defaultOption && <option value={"all"}>전체</option>}
       {options.map((option) => (
         <option key={option.id} value={option.id}>
