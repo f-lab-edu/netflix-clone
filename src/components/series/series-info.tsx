4ac332fa6cf98b48dec3b61211247164008c9516
Big One <sponjjanc@naver.com>
2023-11-23 17:43:37 +0900

    feat: series 장르 카테고리

---
+++ src/components/series/series-info.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import React, { useRef } from "react";
+import { getTvContents } from "@/services/contents";
+import { useInfiniteQuery } from "react-query";
+import { useObserver } from "@/hooks/use-observer";
+import { Contents } from "@/types/browse/types";
+import CardList from "@/components/ui/card-list";
+
+export default function SeriesInfo({ genres }: { genres: string }) {
+  const bottom = useRef<HTMLDivElement | null>(null);
+  const fetchData = async ({ pageParam = 1 }) => {
+    const queryParams = {
+      include_adult: true,
+      include_null_first_air_dates: true,
+      language: "ko",
+      sort_by: "popularity.desc",
+      page: pageParam,
+      with_genres: genres,
+    };
+    return await getTvContents(queryParams);
+  };
+  const { data, fetchNextPage, status } = useInfiniteQuery(
+    ["seriesList"],
+    fetchData,
+    {
+      getNextPageParam: (lastPage) => {
+        const page = lastPage.page;
+        if (lastPage.total_pages === page) return false;
+        return page + 1;
+      },
+    },
+  );
+  const onIntersect = (entries: IntersectionObserverEntry[]) => {
+    entries.forEach((entry) => entry.isIntersecting && fetchNextPage());
+  };
+
+  useObserver({ target: bottom, onIntersect });
+  return (
+    <>
+      {status === "success" && data && (
+        <div className={"grid gap-4 grid-cols-4 px-5"}>
+          {data.pages.map((page: Contents) => (
+            <CardList
+              key={page.page}
+              dataList={page.results}
+              mediaType={page.media_type}
+            />
+          ))}
+        </div>
+      )}
+      <div ref={bottom} className={"flex justify-center my-10"}>
+        <span className="loading loading-spinner loading-md" />
+      </div>
+    </>
+  );
+}
