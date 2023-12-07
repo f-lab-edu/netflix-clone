435db90577afe9782c7239cd331aa894a77bba13
Big One <sponjjanc@naver.com>
2023-12-07 23:26:49 +0900

    feat: my-list 등록/제거

--- src/components/ui/card-item.tsx
+++ src/components/ui/card-item.tsx
@@ -1,15 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
+import StarIcon from "@/components/ui/star";
+import { addMyList } from "@/services/contents";
+import { useUserDispatch, useUserState } from "@/context/user-context";
 
 export default function CardItem({
   id,
   mediaType,
   posterPath,
+  inMyList,
 }: {
   id: number;
   mediaType?: string;
   posterPath?: string;
+  inMyList: boolean;
 }) {
+  const { sessionId } = useUserState();
+  const dispatch = useUserDispatch();
+  const onClickStar = async () => {
+    await addMyList(sessionId, mediaType, id, !inMyList);
+    return inMyList ? dispatch({ type: "REMOVE_MYLIST", id, mediaType }) : dispatch({ type: "ADD_MYLIST", id, mediaType });
+  };
   return (
     <Link
       href={`/contents/${id}?&mediaType=${mediaType}`}
@@ -17,13 +28,25 @@ export default function CardItem({
     >
       <Image
         className={
-          "w-full h-full border rounded cursor-pointer hover:scale-125 "
+          "w-full h-full border rounded cursor-pointer hover:scale-95 "
         }
         src={`https://image.tmdb.org/t/p/w500${posterPath}`}
         alt={"movie-poster"}
         width={500}
         height={500}
       />
+      <div className={"relative"}>
+        <div
+          className={"absolute bottom-0 right-0 mr-3 mb-3"}
+          onClick={onClickStar}
+        >
+          {inMyList ? (
+            <StarIcon key={id} isMyList={true} />
+          ) : (
+            <StarIcon key={id} />
+          )}
+        </div>
+      </div>
     </Link>
   );
 }
