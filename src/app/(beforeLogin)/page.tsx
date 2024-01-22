787730c6c05329013447d8d9698a6a9e2ad307a9
Big One <sponjjanc@naver.com>
2024-01-22 22:08:12 +0900

    :truck: update: 사진으로 업데이트

--- src/app/(beforeLogin)/page.tsx
+++ src/app/(beforeLogin)/page.tsx
@@ -19,9 +19,7 @@ export default function Home() {
         <section>
           <Image
             className="hero h-80vh relative opacity-60 "
-            src={
-              "https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg"
-            }
+            src={"/background.png"}
             alt={"background"}
             width={500}
             height={500}
