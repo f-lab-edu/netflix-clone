cfef3099725dd63603e30862dbe562e11623aeb3
Big One <sponjjanc@naver.com>
2024-02-02 23:25:17 +0900

    :goal_net: fix: signIn callback

--- src/app/(beforeLogin)/login/_component/google-button.tsx
+++ src/app/(beforeLogin)/login/_component/google-button.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react";
 export default function GoogleButton() {
   return (
     <div className={"flex flex-col items-center"}>
-      <button onClick={() => signIn("google", { callbackUrl: "/browse" })}>
+      <button onClick={() => signIn("google")}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="40"
