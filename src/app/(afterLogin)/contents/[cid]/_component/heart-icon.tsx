f6a8a4c78fb37a7fd1829f8ab5a42e734b6dab89
Big One <sponjjanc@naver.com>
2024-02-07 12:41:33 +0900

    :sparkles: feat: 찜한 콘텐츠 아이콘, 찜한 콘텐츠 분기처리

---
+++ src/app/(afterLogin)/contents/[cid]/_component/heart-icon.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import style from "./heart-icon.module.css";
+import { addOrRemoveInMyList } from "@/services/contents";
+export default function HeartIcon({
+  hearted,
+  onFavorite,
+}: {
+  hearted: boolean;
+  onFavorite: () => {};
+}) {
+  return (
+    <div className={style.heartButton}>
+      <button onClick={onFavorite}>
+        {hearted ? (
+          <svg
+            fill="#ff0000"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            stroke="#ff0000"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+            <g
+              id="SVGRepo_tracerCarrier"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></g>
+            <g id="SVGRepo_iconCarrier">
+              <path d="M12 20a1 1 0 0 1-.437-.1C11.214 19.73 3 15.671 3 9a5 5 0 0 1 8.535-3.536l.465.465.465-.465A5 5 0 0 1 21 9c0 6.646-8.212 10.728-8.562 10.9A1 1 0 0 1 12 20z"></path>{" "}
+            </g>
+          </svg>
+        ) : (
+          <svg
+            width={24}
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            className="r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-1plcrui r-lrvibr r-1xvli5t r-1hdv0qi"
+          >
+            <g>
+              <path d="M16.697 5.5c-1.222-.06-2.679.51-3.89 2.16l-.805 1.09-.806-1.09C9.984 6.01 8.526 5.44 7.304 5.5c-1.243.07-2.349.78-2.91 1.91-.552 1.12-.633 2.78.479 4.82 1.074 1.97 3.257 4.27 7.129 6.61 3.87-2.34 6.052-4.64 7.126-6.61 1.111-2.04 1.03-3.7.477-4.82-.561-1.13-1.666-1.84-2.908-1.91zm4.187 7.69c-1.351 2.48-4.001 5.12-8.379 7.67l-.503.3-.504-.3c-4.379-2.55-7.029-5.19-8.382-7.67-1.36-2.5-1.41-4.86-.514-6.67.887-1.79 2.647-2.91 4.601-3.01 1.651-.09 3.368.56 4.798 2.01 1.429-1.45 3.146-2.1 4.796-2.01 1.954.1 3.714 1.22 4.601 3.01.896 1.81.846 4.17-.514 6.67z"></path>
+            </g>
+          </svg>
+        )}
+      </button>
+    </div>
+  );
+}
