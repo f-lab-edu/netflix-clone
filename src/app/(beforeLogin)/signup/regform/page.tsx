1a417474a9d1ca22f694c4b1f133ce5c5e21fb57
Big One <sponjjanc@naver.com>
2024-02-07 12:33:15 +0900

    :sparkles: feat: 회원가입

--- src/app/(beforeLogin)/signup/regform/page.tsx
+++ src/app/(beforeLogin)/signup/regform/page.tsx
@@ -1,47 +1,27 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { CheckInCircle } from "@/components/icon";
-import { useForm } from "react-hook-form";
-import { FormData } from "@/types/signup/types";
-import { signup } from "@/services/account/account";
+import { useFormState, useFormStatus } from "react-dom";
+import onSubmit from "../_lib/signup";
+
+const showKoMessage = (message: string | null) => {
+  if (message === "no_email") return "이메일을 확인해주세요.";
+  if (message === "no_password") return "비밀번호를 확인해주세요.";
+  if (message === "no_required_option")
+    return "개인정보 처리방침에 동의해주셔야합니다.";
+  return "";
+};
 
 function RegFormPage() {
-  const router = useRouter();
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    mode: "onSubmit",
-    defaultValues: {
-      email: "",
-      password: "",
-      requiredTerm: false,
-      optionsTerm: false,
-    },
-  });
-
-  const emailReg =
-    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-
-  const onSubmit = async (data: FormData) => {
-    if (!data.requiredTerm)
-      return alert("개인정보 수집 및 활용 동의는 필수입니다.");
-    const res = await signup(data);
-
-    router.replace("/signup");
-  };
+  const [state, formAction] = useFormState(onSubmit, { message: null });
+  const { pending } = useFormStatus();
 
   return (
     <>
       <div className={"w-full border-t"} />
       <section className={"flex flex-col items-center"}>
         <CheckInCircle />
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className={"border-2 py-16 px-5 rounded-3xl"}
-        >
+        <form action={formAction} className={"border-2 py-16 px-5 rounded-3xl"}>
           <div className={"form-control flex flex-col w-108 "}>
             <h1 className={"text-2xl text-center"}>
               비밀번호를 설정해 멤버십을 <br /> 시작하세요.
@@ -55,32 +35,25 @@ function RegFormPage() {
                 이메일 주소
               </label>
               <input
+                id="email"
+                name="email"
                 className={"input w-full text-black"}
-                {...register("email", {
-                  required: true,
-                  maxLength: 20,
-                  min: 5,
-                  pattern: {
-                    value: emailReg,
-                    message: "이메일 형식에 맞지 않습니다.",
-                  },
-                })}
               />
-              {errors?.email && <small>{errors.email?.message}</small>}
               <input
+                id="password"
+                name="password"
                 className={"input w-full text-black mt-3"}
                 placeholder={"비밀번호를 추가하세요."}
-                {...register("password", {
-                  required: "비밀번호를 입력해주세요",
-                })}
               />
             </div>
             <div className={"terms mt-3"}>
               <div className="flex">
                 <input
+                  id="required_option"
+                  name="required_option"
                   type="checkbox"
                   className="checkbox checkbox-success cursor-pointer"
-                  {...register("requiredTerm")}
+                  required
                 />
                 <p className="pl-3 label-text text-white">
                   예, 저는 <span>개인정보 처리방침</span>에 따른 개인정보 수집
@@ -91,15 +64,23 @@ function RegFormPage() {
                 <input
                   type="checkbox"
                   className="checkbox checkbox-success cursor-pointer"
-                  {...register("optionsTerm")}
                 />
                 <p className="pl-3 label-text text-white ">
-                  예, 넷플릭스 특별 할인 알림 이메일을 보내주세요. (선택 사항)
+                  예, 넷뿌러졌스 특별 할인 알림 이메일을 보내주세요. (선택 사항)
                 </p>
               </div>
             </div>
           </div>
-          <button className={"w-full rounded-md mt-5 p-4 bg-red-700"}>
+          <div>
+            <small className={" text-red-500 mt-1 "}>
+              {showKoMessage(state?.message)}
+            </small>
+          </div>
+          <button
+            type="submit"
+            className={"w-full rounded-md mt-5 p-4 bg-red-700"}
+            disabled={pending}
+          >
             동의하고 계속
           </button>
         </form>
