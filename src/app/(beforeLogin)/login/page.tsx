58a3be65a06f390cf861ac4d25809801b560cce9
Big One <sponjjanc@naver.com>
2024-02-12 21:31:45 +0900

    :sparkles: feat: github 로그인 && 파일명 변경

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -2,7 +2,7 @@
 
 import Footer from "@/app/(beforeLogin)/login/_component/footer";
 import GoogleButton from "@/app/(beforeLogin)/login/_component/google-button";
-import NaverButton from "@/app/(beforeLogin)/login/_component/naver-button";
+import GithubButton from "@/app/(beforeLogin)/login/_component/github-button";
 import Background from "@/app/(beforeLogin)/_component/background";
 import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
@@ -100,7 +100,7 @@ function LoginPage({ searchParams }: { searchParams: { email: string } }) {
           </form>
           <div className={"flex justify-around"}>
             <GoogleButton />
-            <NaverButton />
+            <GithubButton />
           </div>
           <Footer />
         </div>
