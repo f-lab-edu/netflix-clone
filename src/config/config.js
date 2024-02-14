d9849fbbf007b14020cddbf316da214c73f8ff53
Big One <sponjjanc@naver.com>
2024-02-14 22:42:26 +0900

    :truck: move: 파일명 변경 및 이동

--- src/config/config.js
+++
@@ -1,7 +0,0 @@
-export const options = {
-  headers: {
-    accept: "application/json",
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_ACCESS_TOKEN}`,
-    'content-type': 'application/json'
-  },
-};
