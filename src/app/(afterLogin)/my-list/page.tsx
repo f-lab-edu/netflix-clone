dd26c341f63a2a0237eec5c586fa92aa92394d80
Big One <sponjjanc@naver.com>
2024-02-15 15:22:15 +0900

    :sparkles: feat: 서버 컴포넌트로 변경

--- src/app/(afterLogin)/my-list/page.tsx
+++ src/app/(afterLogin)/my-list/page.tsx
@@ -1,34 +1,21 @@
-"use client";
-
 import { getMyListMovie, getMyListTv } from "@/services/contents";
 import CardItem from "@/app/(afterLogin)/_component/card-item";
-import { useUserState } from "@/context/user-context";
 import { Contents } from "@/types/browse/types";
-import { useEffect, useState } from "react";
-import Loading from "@/app/loading";
+import { auth } from "@/auth";
 
-function MyListPage() {
-  //TODO: preloading 사용하여 서버 컴포넌트로 변경
-  const user = useUserState();
-  const myListMoviesData = getMyListMovie(user.sessionId);
-  const myListTvData = getMyListTv(user.sessionId);
-  const [movies, setMovies] = useState<Contents | null>(null);
-  const [tv, setTv] = useState<Contents | null>(null);
-  const getList = async () => {
-    const [movieList, tvList]: [Contents, Contents] = await Promise.all([
-      myListMoviesData,
-      myListTvData,
-    ]);
-    setMovies(movieList);
-    setTv(tvList);
-  };
-  useEffect(() => {
-    getList();
-  }, []);
+async function MyListPage() {
+  const session = await auth();
+  const sessionId = session?.user?.name as string;
 
-  if (!movies && !tv) return <Loading />;
+  const myListMoviesData = getMyListMovie(sessionId);
+  const myListTvData = getMyListTv(sessionId);
 
-  if (movies?.total_results === 0 && tv?.total_results === 0)
+  const [movies, tv]: [Contents, Contents] = await Promise.all([
+    myListMoviesData,
+    myListTvData,
+  ]);
+
+  if (movies.total_results === 0 && tv.total_results === 0)
     return <div>찜한 콘텐츠가 없습니다.</div>;
 
   return (
@@ -37,7 +24,7 @@ function MyListPage() {
         "min-[375px]:grid-cols-2 max-w-[1480px] grid px-5 grid-col-2 gap-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"
       }
     >
-      {movies!.results.map((data) => (
+      {movies.results.map((data) => (
         <CardItem
           key={data.id}
           id={data.id}
@@ -46,7 +33,7 @@ function MyListPage() {
           inMyList={true}
         />
       ))}
-      {tv!.results.map((data) => (
+      {tv.results.map((data) => (
         <CardItem
           key={data.id}
           id={data.id}
