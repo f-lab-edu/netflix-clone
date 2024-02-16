237845c19bb1f33e8e00fc2aeb36f1d6ef1da878
Big One <sponjjanc@naver.com>
2024-02-16 20:59:16 +0900

    :label: add: tv 에만 있는 타입 추가

--- src/model/media.ts
+++ src/model/media.ts
@@ -33,11 +33,14 @@ export interface Media {
   id: number;
   original_language: string;
   original_title: string;
+  original_name: string;
   overview: string;
   popularity: number;
   poster_path: string;
   release_date: string;
+  first_air_date: string;
   title: string;
+  name: string;
   video: boolean;
   vote_average: number;
   vote_count: number;
