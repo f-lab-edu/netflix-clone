7904e0e1284f0fff3c880157a92f543ae614019d
Big One <sponjjanc@naver.com>
2024-02-16 21:01:09 +0900

    :sparkles: feat: 찜한 데이터 가져오기, 추가, 제거 기능

--- src/app/(afterLogin)/_lib/tmdb-api.ts
+++ src/app/(afterLogin)/_lib/tmdb-api.ts
@@ -1,5 +1,5 @@
 import { options } from "@/app/(afterLogin)/_lib/tmdb-config";
-import { Movie, Tv, Video } from "@/model/media";
+import { Media, Movie, Tv, Video } from "@/model/media";
 
 export const getDetail = async (mediaType: string, id: string) => {
   const response = await fetch(
@@ -75,4 +75,55 @@ export const getVideo = async (mediaType: string, id: string) => {
     const result: Video = { key: temp.key, site: temp.site };
     return result;
   }
+};
+
+export async function getFavorite(includePoster?: boolean) {
+  const responseMovies = await fetch(
+    `https://api.themoviedb.org/3/account/${process.env.NEXT_PUBLIC_TMDB_ACCOUNT_ID}/favorite/movies?language=ko&page=1&&sort_by=created_at.asc`,
+    options,
+  ).then((response) => response.json());
+
+  const responseTv = await fetch(
+    `https://api.themoviedb.org/3/account/${process.env.NEXT_PUBLIC_TMDB_ACCOUNT_ID}/favorite/tv?language=ko&page=1&&sort_by=created_at.asc`,
+    options,
+  ).then((response) => response.json());
+
+  console.log(includePoster);
+  if (includePoster) {
+    const movies = responseMovies.results.map((movie: Media) => ({
+      id: movie.id,
+      poster_path: movie.poster_path,
+    }));
+    const tv = responseTv.results.map((t: Media) => ({
+      id: t.id,
+      poster_path: t.poster_path,
+    }));
+    return { movies, tv };
+  }
+  const movies = responseMovies.results.map((movie: Media) => movie.id);
+  const tv = responseTv.results.map((t: Media) => t.id);
+
+  return { movies, tv };
+}
+export const addFavorite = async (
+  mediaType: string,
+  id: string,
+  favorite: boolean,
+) => {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/account/20466738/favorite`,
+    {
+      ...options,
+      method: "POST",
+      body: JSON.stringify({
+        media_type: mediaType,
+        media_id: id,
+        favorite,
+      }),
+    },
+  )
+    .then((response) => response.json())
+    .catch((err) => console.error(err));
+
+  if (!response.success) return;
 };
