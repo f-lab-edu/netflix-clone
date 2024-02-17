1c6e70ee3da87efaf9e07bc2e5be0d58d7bdde9a
Big One <sponjjanc@naver.com>
2024-02-17 21:16:13 +0900

    :label: add: 새로운 검색결과 SearchMedia 타입 추가

--- src/model/media.ts
+++ src/model/media.ts
@@ -49,3 +49,18 @@ export interface Video {
   key: string;
   site: string;
 }
+export interface SearchMedia {
+  adult: boolean;
+  backdrop_path: string;
+  first_air_date: string;
+  genre_ids: number[];
+  id: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  vote_average: number;
+  vote_count: number;
+}
