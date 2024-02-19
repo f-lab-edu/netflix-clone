b4f5b6b47b05e585b245f943de00489e08075b88
Big One <sponjjanc@naver.com>
2024-02-20 00:20:46 +0900

    :necktie: update: tanstack 최신버전 마이그레이션으로 prefetch 적용 및 사용 수정

--- src/app/(afterLogin)/series/[id]/page.tsx
+++ src/app/(afterLogin)/series/[id]/page.tsx
@@ -1,11 +1,33 @@
-import Genres from "@/components/series/genres";
-import SeriesInfo from "@/components/series/series-info";
+import Genres from "@/app/(afterLogin)/series/[id]/_component/genres";
+import TvResults from "@/app/(afterLogin)/series/[id]/_component/tv-results";
+import { getTvSeries } from "@/app/(afterLogin)/series/[id]/_lib/get-tv-series";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import style from "./series.module.css";
 
-export default function SeriesPage({ params }: { params: { id: string } }) {
+export default async function SeriesPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchInfiniteQuery({
+    queryKey: ["tv", "series", params.id],
+    queryFn: () => getTvSeries(params.id, 1),
+    initialPageParam: 1,
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
   return (
-    <section>
-      <Genres params={params.id} />
-      <SeriesInfo genres={params.id} />
-    </section>
+    <HydrationBoundary state={dehydratedState}>
+      <section className={style.container}>
+        <Genres params={params.id} />
+        <TvResults genreId={params.id} />
+      </section>
+    </HydrationBoundary>
   );
 }
