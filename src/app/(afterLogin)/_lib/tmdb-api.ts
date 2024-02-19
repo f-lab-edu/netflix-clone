9116c4b9a98f37a2c421cf4657a9a4f5bcedd9a7
Big One <sponjjanc@naver.com>
2024-02-20 00:14:01 +0900

    :fire: remove: 사용하지 않은 코드 제거

--- src/app/(afterLogin)/_lib/tmdb-api.ts
+++ src/app/(afterLogin)/_lib/tmdb-api.ts
@@ -128,11 +128,3 @@ export const addFavorite = async (
 
   if (!response.success) return;
 };
-
-export const getSearch = async (query: string) => {
-  const response = await fetch(
-    `https://api.themoviedb.org/3/search/multi?query=${query}&include_adult=false&language=ko&page=1`,
-    options,
-  ).then((response) => response.json());
-  return response;
-};
